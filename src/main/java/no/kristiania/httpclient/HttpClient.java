42d83bb0aae9f33724e27d3c9c1fa20d3a3ad9bc
Sarah van Dijk <sarahmurstad@hotmail.com>
2020-09-15 18:09:40 +0200

    HttpClient first test red

--- src/main/java/no/kristiania/httpclient/HttpClient.java
+++ src/main/java/no/kristiania/httpclient/HttpClient.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class HttpClient {
+    public HttpClient(String hostname, int port, String requestTarget) {
+
+    }
+
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("urlecho.appspot.com", 80);
 
@@ -24,5 +28,9 @@ public class HttpClient {
             // Treat each byte as a character ("(char)") and print it to the console
             System.out.println((char)c);
         }
+    }
+
+    public int getResponseCode() {
+        return 0;
     }
 }
